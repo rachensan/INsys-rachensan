45336d3577f3aec5c7855ab3f3a0160ee6a5c176
rachensan <lilamarino333@gmail.com>
2025-10-08 20:44:14 +0800

    redisClient.js

--- backend/utils/redisClient.js
+++ backend/utils/redisClient.js
@@ -9,13 +9,16 @@
 // await redisClient.connect();
 
 // export default redisClient;
-
 import { createClient } from "redis";
 import dotenv from "dotenv";
 dotenv.config();
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL, // no TLS here
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,               // Required if using rediss:// or Render TLS
+    rejectUnauthorized: false
+  },
 });
 
 redisClient.on("error", (err) => console.error("Redis Client Error:", err));
@@ -23,3 +26,5 @@ redisClient.on("error", (err) => console.error("Redis Client Error:", err));
 await redisClient.connect();
 
 export default redisClient;
+
+
