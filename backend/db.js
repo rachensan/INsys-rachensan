9390133292fbfc201a0313c0aee76016d3356673
rachensan <lilamarino333@gmail.com>
2025-10-08 19:20:50 +0800

    db.js

--- backend/db.js
+++ backend/db.js
@@ -3,13 +3,19 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './.env', quiet: true });
 
 export const db = new pg.Client({
-  
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD, 
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // required by Render
 });
+
+
+// export const db = new pg.Client({
+  
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD, 
+//   port: process.env.DB_PORT,
+// });
 
 db.connect();
 
